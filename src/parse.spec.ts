a1ba71d89e87e8d177d43cd41f9f40d57300aafe
Scott Fasone <scott181182@yahoo.com>
2025-09-22 21:25:40 -0400

    test: updated parse functions to run on Blobs and added tests for them

---
+++ src/parse.spec.ts
@@ -0,0 +1,44 @@
+import { describe, it, expect } from "bun:test"
+import { parseDomains, parseTraeficDomains } from "./parse";
+
+
+
+describe("parseTraeficDomains", () => {
+    const TRAEFIK_RULE_CONTENT = `
+        Host(\`traefik.home\`)&&PathPrefix(\`/api\`)
+        Host(\`traefik.home\`)
+        Host(\`grist.home\`)
+        Host(\`pihole.home\`)
+    `;
+
+    it("should parse traefik router rules", async () => {
+        const blob = new Blob([TRAEFIK_RULE_CONTENT])
+        const res = await parseTraeficDomains(blob);
+
+        expect(res).toEqual([
+            "traefik.home",
+            "grist.home",
+            "pihole.home",
+        ]);
+    });
+});
+
+describe("parseDomains", () => {
+    const WHITELIST_CONTENT = `
+        something.random.home
+
+        pihole.home
+        example.local
+    `;
+
+    it("should parse domains", async () => {
+        const blob = new Blob([WHITELIST_CONTENT])
+        const res = await parseDomains(blob);
+
+        expect(res).toEqual([
+            "something.random.home",
+            "pihole.home",
+            "example.local",
+        ]);
+    });
+});
